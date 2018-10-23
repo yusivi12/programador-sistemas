0aded9ca61f978a5c8ecdb1dfbaf243b04a8b176
Yuri <44246511+yusivi12@users.noreply.github.com>
2018-10-22 21:35:24 -0300

    Add files via upload

---
+++ Aula 7/005_TiposPrimitivos/src/controller/ExemploInteiro2naooficial.java
@@ -0,0 +1,16 @@
+package controller;
+
+public class ExemploInteiro2naooficial {
+	public static void main (String[] args) {
+		int numero1, numero2;
+		float numero3, soma;
+		numero1 = 12;
+		numero2 = 3;
+		numero3 = 4;
+		soma = numero1 + numero2 + numero3;
+		
+		System.out.println("Valor da Soma: "+ soma);
+	}
+
+
+}
