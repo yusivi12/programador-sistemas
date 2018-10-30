ad093cbbfe7677242d77bd81fe221bbe4d473b8c
Yuri <44246511+yusivi12@users.noreply.github.com>
2018-10-29 21:39:59 -0300

    Add files via upload

---
+++ Aula 14/011_Herança/src/model/Professor.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Professor extends Pessoa {
+	private String matricula;
+	private String segmento;
+	
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula (String matricula) {
+		this.matricula = matricula;
+	}
+	public void getSegmento() {
+		return segmento;
+	}
+	public void setSegmento(String segmento) {
+	}
+public Professor() {
+}
